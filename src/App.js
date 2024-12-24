bf6fd1dfed7d9b4c918dec652740d5d9ed1110a1
Souradip Ghosh <souradip1212@gmail.com>
2024-12-24 21:24:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -83,7 +83,9 @@ const App = () => {
             <h2>Submitted Data:</h2>
             <p><strong>Name:</strong> {submittedData.name}</p>
             <p><strong>Email:</strong> {submittedData.email}</p>
-            <p><strong>Country:</strong> {submittedData.country}</p>
+            <p><strong>Country:</strong> 
+              <div dangerouslySetInnerHTML={{ __html: submittedData.country }} />
+            </p>
             <p>
               <strong>Description:</strong>{" "}
               <div
